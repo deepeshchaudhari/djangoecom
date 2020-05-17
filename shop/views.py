f0461a85702230e022b9d4ec5372a8160c945d20
Deepesh Chaudhari <deepeshc@cse.iitk.ac.in>
2020-05-17 15:02:02 +0530

    "slider and product category wise added"

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,22 @@
+from math import ceil
+
 from django.shortcuts import render
 from django.http import HttpResponse
+
+# import models or database
+from .models import Product
 # Create your views here.
 def index(request):
-    return render(request,'shop/index.html')
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nslides= ceil(n/4)
+        allProds.append([prod, range(1, nslides), nslides])
+    params = {'allProds': allProds}
+    return render(request,'shop/index.html',params)
 def about(request):
     return render(request,'shop/about.html')
 def contact(request):
